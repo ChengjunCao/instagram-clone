51d78d46f6d59d687e0c41474cf3ca6f61be0719
Mike Cao <mikecao369@gmail.com>
2021-04-12 15:29:50 -0400

    update

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import * as firebase from 'firebase';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './redux/reducers';
+import thunk from 'redux-thunk';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBT3UcO_e4k4b9ICqUsWUYLej99mQO0-70",
+  authDomain: "instagram-c8945.firebaseapp.com",
+  projectId: "instagram-c8945",
+  storageBucket: "instagram-c8945.appspot.com",
+  messagingSenderId: "825820538573",
+  appId: "1:825820538573:web:2986c11e544fac00d8eb84",
+  measurementId: "G-WC3YYRLH4G"
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+import MainScreen from './components/Main';
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if(!loggedIn){
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Landing'>
+            <Stack.Screen name='Landing' component={LandingScreen} options={{ headerShown: false }}/>
+            <Stack.Screen name='Register' component={RegisterScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return(
+      <Provider store={store}>
+        <MainScreen />
+      </Provider>
+
+    )
+  }
+}
+
+export default App
