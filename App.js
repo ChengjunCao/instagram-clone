c46ca9fd67308bfd8e83695d102034a159a9f878
Mike Cao <mikecao369@gmail.com>
2021-04-18 16:22:45 -0400

    add feed profile button

--- App.js
+++ App.js
@@ -28,8 +28,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
+import LoginScreen from './components/auth/Login';
 import MainScreen from './components/Main';
-
+import AddScreen from './components/main/Add'
 const Stack = createStackNavigator();
 
 export class App extends Component {
@@ -71,6 +72,7 @@ export class App extends Component {
           <Stack.Navigator initialRouteName='Landing'>
             <Stack.Screen name='Landing' component={LandingScreen} options={{ headerShown: false }}/>
             <Stack.Screen name='Register' component={RegisterScreen}/>
+            <Stack.Screen name='Login' component={LoginScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       );
@@ -78,7 +80,12 @@ export class App extends Component {
 
     return(
       <Provider store={store}>
-        <MainScreen />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Main'>
+            <Stack.Screen name='Main' component={MainScreen} options={{ headerShown: false }}/>
+            <Stack.Screen name='Add' component={AddScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
 
     )
